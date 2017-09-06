8a22ea6b4034a9f61da93e7246fd6b9c4819fc70
Hugo Alliaume <kocal@live.fr>
2017-09-06 12:58:49 +0200

    Modification de la structure du projet

--- watch-and-build.js
+++ watch-and-build.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 
-const chokidar = require('chokidar');
+const path = require('path');
 const exec = require('child_process').exec;
+const chokidar = require('chokidar');
 
-chokidar.watch('*.odt')
+const libreofficePath = `file://${path.join(__dirname, '.libreoffice')}/`;
+const outputPath = path.join(__dirname, 'dist');
+
+
+chokidar.watch('src/*.odt')
   .on('change', (path) => {
-    exec('libreoffice -env:UserInstallation=file:///home/kocal/.config/libreoffice-alt --headless --convert-to pdf *.odt', (err, stdout, stderr) => {
+    exec(`libreoffice -env:UserInstallation=${libreofficePath} --headless --convert-to pdf --outdir "${outputPath}" src/*.odt`, (err, stdout, stderr) => {
       if(err) {
         throw err;
       }
