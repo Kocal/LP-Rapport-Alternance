7ecd439f3bd0c4db7c1af51437f50e106901e37c
Hugo Alliaume <kocal@live.fr>
2017-09-06 19:13:35 +0200

    Update watch-and-build.js

--- watch-and-build.js
+++ watch-and-build.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 
+const os = require('os');
 const path = require('path');
 const exec = require('child_process').exec;
 const chokidar = require('chokidar');
 
-const libreofficePath = `file://${path.join(__dirname, '.libreoffice')}/`;
+const libreofficePath = `file://${path.join(os.tmpdir(), '.libreoffice')}/`;
 const outputPath = path.join(__dirname, 'dist');
 
 
 chokidar.watch('src/*.odt')
   .on('change', (path) => {
-    exec(`libreoffice -env:UserInstallation=${libreofficePath} --headless --convert-to pdf --outdir "${outputPath}" src/*.odt`, (err, stdout, stderr) => {
+    exec(`libreoffice -env:UserInstallation="${libreofficePath}" --headless --convert-to pdf --outdir "${outputPath}" src/*.odt`, (err, stdout, stderr) => {
       if(err) {
         throw err;
       }
